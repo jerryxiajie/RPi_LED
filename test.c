4e990351baf21d87b20c5ee5313c49f5b24d3c4a
Jerry_xia <Jerry.xia@embest-tech.com>
2019-03-20 18:08:52 +0800

    test

--- test.c
+++ test.c
@@ -12,11 +12,11 @@ float readFile(void)
 {
 	int i,read_len;
         FILE * file_fd;
-        char * FILE_NAME = "sys/bus/w1/devices/28-020592461ab5/w1_slave";
-        unsigned char file_buffer[128];
+        char * FILE_NAME = "/sys/bus/w1/devices/28-020592461ab5/w1_slave";
+        unsigned char file_buffer[256];
         float temp;
 
-        file_fd = fopen(FILE_NAME,O_RDONLY);
+        file_fd = fopen(FILE_NAME,"r");
         if(file_fd == NULL)
         {
                 printf("File open failed! \n");
@@ -27,18 +27,16 @@ float readFile(void)
                 printf("File open success! \n");
         }
 
-        fseek(file_fd,0L,SEEK_END);
-        read_len=ftell(file_fd);
-        fseek(file_fd,0L,SEEK_SET);
-        fread(file_buffer,read_len,1,file_fd);
+        fread(file_buffer,128,1,file_fd);
         fclose(file_fd);
-        file_buffer[read_len]=0;
         i = 0;
         while(file_buffer[i++]  != 't');
         temp = atof(&file_buffer[i+1]);
-	printf("temp = %f \n",temp);
 
-	return temp/1000;
+	printf("temp = %f", temp);
+
+        return temp/1000; 
+
 }
 
 int main(void)
